30fd33e0670fd5db918b5516673a59f360e325ac
Sean McLeaish <smcleaish@gmail.com>
2025-03-06 22:21:00 -0600

    chapter 2 echor, completed builder example, started derive example

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,5 +1,20 @@
-use std::env::args;
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+/// Rust version of `echo`
+
+struct Args {
+    /// Input text
+    #[arg(required(true))]
+    text: Vec<String>,
+
+    /// Do not print newline
+    #[arg(short('n'))]
+    omit_newline: bool,
+}
 
 fn main() {
-    println!("{:?}", args());
+    let args = Args::parse();
+    dbg!(args);
 }
