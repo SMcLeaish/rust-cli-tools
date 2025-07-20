beff9221a1bb7a1fffd08e4d611478fa90b5a2c0
Sean McLeaish <smcleaish@gmail.com>
2025-07-20 17:15:31 -0400

    Trying to sort out the arguments

--- headr/src/main.rs
+++ headr/src/main.rs
@@ -1,6 +1,23 @@
-#[derive(Debug)]
+use anyhow::Result;
+use clap::Parser;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+/// Rust version of `head`
 struct Args {
+    /// Target files
+    #[arg(value_name = "FILE", default_value = "-")]
     files: Vec<String>,
+    /// Print n lines [default: 10]
+    #[arg(short('n'), long("lines"), default_value = "10")]
     lines: u64,
+    /// Print n bytes
     bytes: Option<u64>,
+}
+
+fn main() {
+    let args = Args::parse();
+    println!("{:#?}", args)
 }
