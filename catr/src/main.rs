b1095df201d3134c3ecfc35bda3fea29295971fc
Sean McLeaish <smcleaish@gmail.com>
2026-04-13 18:56:06 -0400

    passing 10

--- catr/src/main.rs
+++ catr/src/main.rs
@@ -1,5 +1,7 @@
 use anyhow::Result;
 use clap::Parser;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 
 #[derive(Debug, Parser)]
 #[command(author, version, about)]
@@ -18,10 +20,30 @@ struct Args {
 }
 
 fn main() {
-    let args = Args::parse();
-    println!("{args:#?}");
+    if let Err(e) = run(Args::parse()) {
+        eprintln!("{e}");
+        std::process::exit(1);
+    }
 }
 
-fn run(_args: Args) -> Result<()> {
+fn run(args: Args) -> Result<()> {
+    for filename in args.files {
+        match open(&filename) {
+            Err(err) => eprintln!("Failed to open {filename}: {err}"),
+            Ok(file) => {
+                for line_result in file.lines() {
+                    let line = line_result?;
+                    println!("{line}");
+                }
+            }
+        }
+    }
     Ok(())
+}
+
+fn open(filename: &str) -> Result<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
 }
